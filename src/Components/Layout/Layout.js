8f4e052228b635bcbe00179c7b5fe411879cd03a
Abhinav Rao <abhinavrao941@gmail.com>
2019-01-25 04:12:12 +0530

    creating practice project

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Layout.css'
+
+const Layout =(props)=>{
+    return(
+        <div>
+            <h3>Header</h3>
+            <main>to do controls</main>
+            <footer>footer</footer>
+        </div>
+    );
+}
+export default Layout
